e9b5e6c199e5672c0f3b59dc2b8a2cd820c2f00e
trevor-james-nangosha <trevornangosha16@gmail.com>
2022-04-21 09:56:02 +0300

    figured out how to use promises with the hash function. i put the db creation logic inside the then block

--- server.js
+++ server.js
@@ -4,14 +4,25 @@ import connectDB from "./config/dbConn.js";
 import Student from "./models/Student.js";
 import session from 'express-session';
 import MongoStore from "connect-mongo";
+import cors from 'cors'
+import bcrypt from 'bcrypt'
+import { generateRegistrationNumber } from "./utilities/utilities.js";
+import { generateStudentNumber } from "./utilities/utilities.js";
+
+//TODO
+//how to set up ejs for template rendering
+//look at the mongoDB error codes and how to fix them
+//download the TODO VSCode extension
+//look at the E11000 that is making my mongodb code fail
 
 //defining the consonants
 const app =  express();
 const port = process.env.PORT || 3000
 
-//the middleware
 app.use(json())
 app.use(urlencoded())
+app.set('view engine', 'ejs');
+app.use(cors())
 app.use(session({
     secret: "the sessions secret",
     store: MongoStore.create({
@@ -25,32 +36,32 @@ app.use(session({
     }
 }))
 
-//connecting to the DB
 connectDB();
 
-//the routes
-//this is the home page for logged in users
 app.get('/', (req, res) => {
     if(req.session.isAuth){
         res.status(200).send("welcome to the home page")
     }else{
-        res.status(200).send("you cannot access the home page......")
+        res.status(200).send("please login to access site services......")
     }
 })
 
-// API to return a JSON of all the students
 app.get('/students', (req, res) => {
-    Student.find((error, students) => {
-        if(!error){
-            res.json(students)
-        }else{
-            console.log(error)
-        }
-    })
+    if(req.session.isAuth){
+        Student.find((error, students) => {
+            if(!error){
+                res.json(students)
+            }else{
+                console.log(error)
+            }
+        })
+    }else{
+        res.redirect('/');
+    }
+    
 }) 
 
-//API to return a particular user depending on their studentID
-app.get('/students/:studentID', (req, res) => {
+app.get('/v1/students/:studentID', (req, res) => {
     Student.findOne(req.params, (error, student) => {
         if(!error){
             if(!student){
@@ -77,23 +88,7 @@ app.get('/students/:studentID/course/:courseName', (req, res) => {
             console.log(error)
         }
     })
-    // res.send(req.params)
 })
-
-//TODO
-//check if mongoDB allows a data type of object,
-// like if you want to have something like this
-//course: {
-//     courseID: "MUK2918",
-//     courseName: "Bachelor of Science in Mechanical Engineering"
-// }
-
-
-
-//our dummy form
-const provideLoginDetails = (studentNumber, email) => {
-    return [studentNumber, email]
-}
 
 const loginAuthenticatedUser = (req, res) => {
     req.session.isAuth = true;
@@ -113,6 +108,9 @@ const authenticateUser = (studentID, email) => {
         }
     })
     return user
+    //this thing is authenticating even for an empty database
+    //TODO
+    //fix that
 }
 
 app.get('/login', (req, res) => {
@@ -126,11 +124,44 @@ app.get('/login', (req, res) => {
     }
 })
 
+
+//something is wrong here
+//the student model is not creating and saving the database instance
+app.post('/students', async (req, res) => {
+    const {password, firstName, lastName, email} = req.body       
+    try {
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt).then((hashedPassword) => {
+            const newStudent = new Student({
+                studentNumber: generateStudentNumber(),
+                registrationNumber: generateRegistrationNumber(),
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: hashedPassword
+            })
+            console.log(newStudent)
+            newStudent.save((error, student) => {
+                if(!error){
+                    console.log("\nstudent saved.\n")
+                }else{
+                    console.log(`\n this is the mongoDB error handling: `)
+                    console.error(`${error}\n`)
+                }
+            })
+        }).catch((error) => {
+            console.log(`\n this is the promise error handling: `)
+            console.error(`${error}\n`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
 //for invalid 404 requests
 app.get('*', (req, res) => {
     res.status(404).send("Page not found....")
 })
-
 
 mongoose.connection.once("open", () => {
     console.log("connected to mongoDB..........");
